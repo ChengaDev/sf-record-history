7e7eba6a2c9782a9cc67e906a7979b7646ec8cb6
Chen Gazit <gazit.chen@gmail.com>
2020-08-01 02:18:06 +0300

    some branding

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 /*global chrome*/
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './Styles/App.css';
 
 function App() {
-  const [currentUrl, setCurrentUrl] = useState('');
+    const [currentUrl, setCurrentUrl] = useState('');
+    const backupId = 123123;
 
-  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-      let url = tabs[0].url;
-      // use `url` here inside the callback because it's asynchronous!
-      setCurrentUrl(url);
-  });
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+        let url = tabs[0].url;
+        // use `url` here inside the callback because it's asynchronous!
+        setCurrentUrl(url);
+    });
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          OwnBackup extension to view SalesForce record
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
+                <div>Backup {backupId} records changes</div>
+                <div>{currentUrl}</div>
+            </header>
         </div>
-        <div>
-          {currentUrl}
-        </div>
-      </header>
-    </div>
-  );
+    );
 }
 
 export default App;
